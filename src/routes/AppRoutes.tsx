1c25fe2185da63a39e5f913a9d57f47dd19228bf
Juan Enriquez <juan.enriquezc@campusucc.edu.co>
2025-10-15 16:29:13 -0500

    feat: add AppRoutes component with structured routing

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,24 +1,28 @@
 import { Routes, Route } from "react-router-dom";
+
+// Components
 import Layout from "../components/Layout";
 
 // Views
-import HomePage from "../views/HomePage";
-import ThreeDemoView from "../views/ThreeDemoView";
-import LayoutsView from "../views/LayoutsView";
-import SpeechDemoView from "../views/SpeechDemoView";
+import ClickCounterView from "../views/ClickCounterView";
 import GeometryExplorer from "../views/GeometryExplorer";
+import HomePage from "../views/HomePage";
+import LayoutsView from "../views/LayoutsView";
 import SettingsView from "../views/SettingsView";
+import SpeechDemoView from "../views/SpeechDemoView";
+import ThreeDemoView from "../views/ThreeDemoView";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="three" element={<ThreeDemoView />} />
+        <Route path="click" element={<ClickCounterView />} />
+        <Route path="geometry" element={<GeometryExplorer />} />
         <Route path="layouts" element={<LayoutsView />} />
-        <Route path="tts" element={<SpeechDemoView />} />
-        <Route path="three_2" element={<GeometryExplorer />} />
         <Route path="settings" element={<SettingsView />} />
+        <Route path="three" element={<ThreeDemoView />} />
+        <Route path="tts" element={<SpeechDemoView />} />
       </Route>
     </Routes>
   );
