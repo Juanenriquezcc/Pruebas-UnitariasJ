84b60b792cb0b46988f46b392bb33afce9747eb2
Juan Enriquez <juan.enriquezc@campusucc.edu.co>
2025-10-18 14:55:53 -0500

    feat: agrega vista ClickCounterView con contador funcional

---
+++ src/views/ClickCounterView.tsx
@@ -0,0 +1,10 @@
+import ClickCounter from "../components/ClickCounter";
+
+export default function ClickCounterView() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <ClickCounter />
+    </div>
+  );
+}
+
